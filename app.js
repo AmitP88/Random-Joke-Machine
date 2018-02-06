d1a6668f9b8c56502d6b25f142efb7f962d7eb41
Amit Patel <amitpatel_7@msn.com>
2018-02-06 04:04:00 -0800

    used a joke api to generate random jokes
    The joke api was created by David Katz and is found here: https://github.com/15Dkatz/official_joke_api
    I found it on Google

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 
     $("#generate").on("click", function() {
-        $(".quote").html("Here is the quote");
+
+        $.getJSON("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke", function(json) {
+            $(".quote").html(JSON.stringify(json));
+          });
+
+
+
+
+
     });
 
 
