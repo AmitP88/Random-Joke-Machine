c861b7a00edaf53ac4b1e49f5cb567b4621cc0e6
Amit Patel <amitpatel_7@msn.com>
2018-02-15 03:06:50 -0800

    removed console.logs and extra whitespace

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 $(document).ready(function() {
-
     var joke = '';
-
     $("#generate").on("click", function() {
-
         $.getJSON("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke", function(json) {
-            // $(".quote").html(JSON.stringify(json));
-
-            // console.log(json);
-
             joke = json.setup + " " + json.punchline;
-
-            console.log(joke);
-
             $(".setup").html("Question : " + JSON.stringify(json.setup));
-
             $(".punchline").html("Answer : " + JSON.stringify(json.punchline));
-
             $(".twitter-share-button").attr('href', 'https://twitter.com/intent/tweet?text='+joke).attr('target', '_blank');
-
           });
     });
-
 });
