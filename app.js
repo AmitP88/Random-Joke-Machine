008319e0193ec0efd3343f567cfa014f880d8b0d
Amit Patel <amitpatel_7@msn.com>
2018-04-05 12:31:55 -0700

    changed style of the entire app

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     $("#generate").on("click", function() {
         $.getJSON("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke", function(json) {
             joke = json.setup + " " + json.punchline;
-            $(".setup").html("Question : " + JSON.stringify(json.setup));
-            $(".punchline").html("Answer : " + JSON.stringify(json.punchline));
+            $(".setup").html("Joke : " + JSON.stringify(json.setup));
+            $(".punchline").html("Punchline : " + JSON.stringify(json.punchline));
             $(".twitter-share-button").attr('href', 'https://twitter.com/intent/tweet?text='+joke).attr('target', '_blank');
           });
     });
